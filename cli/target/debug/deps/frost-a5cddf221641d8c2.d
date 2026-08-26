7a3dd312a3e4beebbf010b03802c2234c5bb05ce
ArtFaz <arthur.fazioni@hotmail.com>
2026-08-26 20:13:54 -0300

    chore: exclude cargo build artifacts

--- cli/target/debug/deps/frost-a5cddf221641d8c2.d
+++
@@ -1,7 +0,0 @@
-/home/art/Frosted-Glass/frost/cli/target/debug/deps/frost-a5cddf221641d8c2.d: src/main.rs
-
-/home/art/Frosted-Glass/frost/cli/target/debug/deps/frost-a5cddf221641d8c2: src/main.rs
-
-src/main.rs:
-
-# env-dep:CARGO_PKG_VERSION=0.1.0
